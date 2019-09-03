40bebbc19092d1c04939d2a305348a2e58baed04
akshit3797 <smittal3797@gmail.com>
2019-09-03 23:41:13 +0530

    Login and Register Completed

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
 from config import app_config
 # db variable initialization
 db = SQLAlchemy()
@@ -15,6 +16,7 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
+    Bootstrap(app)
     db.init_app(app)
 
     # # temporary route
@@ -27,6 +29,7 @@ def create_app(config_name):
     login_manager.login_view = "auth.login"
 
     migrate= Migrate(app,db)
+
 
     from app import models
 
